6b043038c11c2a882e33e92fb54afc502005e767
doomertheboomer <hackerdoomer@gmail.com>
2026-05-30 11:28:33 +0700

    add shoot function (untested)

--- core/src/main/java/com/jjs/game/screens/gameplay/Character.java
+++ core/src/main/java/com/jjs/game/screens/gameplay/Character.java
@@ -128,6 +128,80 @@ public abstract class Character {
         return true;
     }
 
+    // raycast algo for hitscan
+    protected void shoot(float angle, int damage) {
+        final float STEP = 4f; // resolution
+
+        // ray start coords and angle mults (yay trig)
+        float px = x + 24;
+        float py = y + 24;
+        float dx = (float) Math.cos(angle);
+        float dy = (float) Math.sin(angle);
+
+        int[] prevTile = Functions.pixelToTile(px, py, false);
+        int prevTileX = prevTile[0];
+        int prevTileY = prevTile[1];
+
+        // keep casting until break
+        while (true) {
+            // move ray
+            px += dx * STEP;
+            py += dy * STEP;
+
+            // current tile ray is in
+            int[] tilecoords = Functions.pixelToTile(px, py, false);
+            int tileX = tilecoords[0];
+            int tileY = tilecoords[1];
+
+            // ray oob
+            if (tileX < 0 || tileY < 0 || tileX >= Constants.MAP_SIZE || tileY >= Constants.MAP_SIZE) {
+                return;
+            }
+
+            // check for walls during tile boundary crosses
+            Tile tile = world.getTile(prevTileX, prevTileY);
+            if (tile != null) {
+                // east wall
+                if (tileX > prevTileX && tile.hasWall(Constants.Direction.EAST)) {
+                    world.setWall(prevTileX, prevTileY, Constants.Direction.EAST, false);
+                    return;
+                }
+                // west wall
+                if (tileX < prevTileX && tile.hasWall(Constants.Direction.WEST)) {
+                    world.setWall(prevTileX, prevTileY, Constants.Direction.WEST, false);
+                    return;
+                }
+                // north wall
+                if (tileY > prevTileY && tile.hasWall(Constants.Direction.NORTH)) {
+                    world.setWall(prevTileX, prevTileY, Constants.Direction.NORTH, false);
+                    return;
+                }
+                // south wall
+                if (tileY < prevTileY && tile.hasWall(Constants.Direction.SOUTH)) {
+                    world.setWall(prevTileX, prevTileY, Constants.Direction.SOUTH, false);
+                    return;
+                }
+            }
+
+            // character hit
+            for (Character c : entities) {
+                // ignore self
+                if (c == this) {
+                    continue;
+                }
+                // check if ray is within 48x48px hitbox
+                if (px >= c.x && px <= c.x + 48 && py >= c.y && py <= c.y + 48) {
+                    c.hp -= damage;
+                    return;
+                }
+            }
+
+            // prepare for next iteration
+            prevTileX = tileX;
+            prevTileY = tileY;
+        }
+    }
+
     public abstract void update(float dt);
 
     public void render(SpriteBatch batch) {
